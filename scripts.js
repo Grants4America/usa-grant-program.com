8e88c5f28d5eb898794f05db9ab2fda56f0936c3
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-08-30 01:49:26 +0100

    Updated

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Any JavaScript you need to initialize goes here
 });
+
+const testimonies = document.querySelectorAll(".testimony");
+const nextButton = document.getElementById("nextTestimony");
+let currentTestimony = 0;
+
+nextButton.addEventListener("click", () => {
+  testimonies[currentTestimony].style.transform = `translateX(-${
+    100 * (currentTestimony + 1)
+  }%)`;
+  currentTestimony = (currentTestimony + 1) % testimonies.length;
+  testimonies[currentTestimony].style.transform = `translateX(-${
+    100 * currentTestimony
+  }%)`;
+});
